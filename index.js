a54eda6755ad5f2fe98a12ce017a3a95e43c6f50
Alex Yiu <alex13920.cep@gmail.com>
2021-05-20 21:47:47 +0800

    16. request api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const exphbs  = require('express-handlebars');
 const app = express()
 const path  =require('path')
 const PORT = process.env.PORT || 5000
+const request= require("request")
 
 //set handlebars middleware 
 app.engine('handlebars', exphbs());
@@ -12,10 +13,16 @@ app.set('view engine', 'handlebars');
 const otherstuff= "hello, there this is other stuff"
 //set handlebar routes
 app.get('/', function (req, res) {
-    res.render('home', {stuff: otherstuff});
+    res.render('home'
+    , {stuff: otherstuff});
+});
+
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 // set static folder
 app.use(express.static(path.join(__dirname, "public")))
 app.listen(PORT, () => console.log("server listening"))
 
+// API key pk_c5b127615d0b4eb9925735138dc49fae
