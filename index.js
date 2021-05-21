5f9d4ed90664b4bfddc600182682e967c0abd51f
Alex Yiu <alex13920.cep@gmail.com>
2021-05-21 12:04:41 +0800

    22 done

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const app = express()
 const path  =require('path')
 const PORT = process.env.PORT || 5000
 const request= require("request")
-
+const bodyParser= require('body-parser')
 
 // API key pk_c5b127615d0b4eb9925735138dc49fae
+
+//use body parser middleware 
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 
 //create call_api function 
@@ -24,15 +28,25 @@ function call_api(finishedAPI){
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+
+//set handlebar index Get route
 app.get('/', function (req, res) {
     call_api(function(doneAPI){
         res.render('home', {
             stock: doneAPI
         });
     });
-
-    
 });
+// set handlebar index post route
+app.post('/', function (req, res) {
+    call_api(function(doneAPI){
+        //posted_stuff= req.body.stock_ticker
+        res.render('home', {
+            stock: doneAPI, 
+        });
+    });
+});
+
 
 app.get('/about.html', function (req, res) {
     res.render('about');
